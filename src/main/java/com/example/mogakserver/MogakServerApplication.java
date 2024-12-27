fae1c27fd7a5f3ebe142eda6c4ec972348c9b553
pkl0912 <pklpkl0912@gmail.com>
2024-12-27 18:57:53 +0900

    first init

---
+++ src/main/java/com/example/mogakserver/MogakServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mogakserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogakServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MogakServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mogakserver/MogakServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mogakserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogakServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
