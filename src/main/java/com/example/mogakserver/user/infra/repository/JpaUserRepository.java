63934e1da50132928f93b0bde1ebe911912ac1b0
kimzini <rlawlsdl11111@gmail.com>
2025-01-13 19:40:33 +0900

    [remove] userRepository 삭제

--- src/main/java/com/example/mogakserver/user/infra/repository/JpaUserRepository.java
+++ src/main/java/com/example/mogakserver/user/infra/repository/JpaUserRepository.java
@@ -4,11 +4,10 @@ import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.example.mogakserver.user.domain.entity.User;
-import com.example.mogakserver.user.domain.repository.UserRepository;
 
 import java.util.Optional;
 
-public interface JpaUserRepository extends UserRepository, JpaRepository<User, Long> {
+public interface JpaUserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.kakaoId = :kakaoId")
     Optional<User> findByKakaoId(@Param("kakaoId") Long kakaoId);
 }
