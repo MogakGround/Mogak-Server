4dd69a7adf77d8692f56831aad8012fb5ea6f963
pkl0912 <pklpkl0912@gmail.com>
2025-01-27 19:15:09 +0900

    [feat] 타이머 시작/종료 기능 추가

--- src/main/java/com/example/mogakserver/external/socket/WebRtcWebSocketHandler.java
+++ src/main/java/com/example/mogakserver/external/socket/WebRtcWebSocketHandler.java
@@ -1,7 +1,12 @@
 package com.example.mogakserver.external.socket;
 
 import com.example.mogakserver.common.config.jwt.JwtService;
+import com.example.mogakserver.common.exception.enums.ErrorCode;
+import com.example.mogakserver.common.exception.model.NotFoundException;
 import com.example.mogakserver.external.socket.dto.MessageDTO;
+import com.example.mogakserver.roomuser.infra.repository.JpaRoomUserRepository;
+import com.example.mogakserver.user.domain.entity.User;
+import com.example.mogakserver.user.infra.repository.JpaUserRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +32,8 @@ public class WebRtcWebSocketHandler extends TextWebSocketHandler {
     private final JwtService jwtService;
     private final MessageListenerAdapter listenerAdapter;
     private final WebSocketBroadCaster webSocketBroadcaster;
+    private final JpaUserRepository userRepository;
+    private final JpaRoomUserRepository roomUserRepository;
 
     private static final String CHANNEL_PREFIX = "room-";
     private static final String SCREEN_SHARE_KEY_PREFIX = "screen-share-room-";
@@ -90,6 +97,16 @@ public class WebRtcWebSocketHandler extends TextWebSocketHandler {
             case "screen-share-stop":
                 publishToRedis(roomId, eventMessage);
                 removeScreenShareUser(roomId, userId);
+                webSocketBroadcaster.broadcast(roomId, eventMessage);
+                break;
+            case "timer-start":
+                publishToRedis(roomId, eventMessage);
+                startTimer(roomId, userId);
+                webSocketBroadcaster.broadcast(roomId, eventMessage);
+                break;
+            case "timer-stop":
+                publishToRedis(roomId, eventMessage);
+                stopTimer(roomId, userId);
                 webSocketBroadcaster.broadcast(roomId, eventMessage);
                 break;
 
@@ -182,6 +199,56 @@ public class WebRtcWebSocketHandler extends TextWebSocketHandler {
 
     private MessageDTO createEventMessage(String type, Long userId) {
         return new MessageDTO(type, userId);
+    }
+
+    private void startTimer(Long roomId, Long userId) {
+        String key = "timer-room-" + roomId;
+
+        //경과시간 초기화
+        String elapsedTimeStr = (String) redisTemplate.opsForHash().get(key, userId + "-elapsedTime");
+        long elapsedTime = (elapsedTimeStr != null) ? Long.parseLong(elapsedTimeStr) : 0;
+
+        long startTime = System.currentTimeMillis();
+        redisTemplate.opsForHash().put(key, userId + "-startTime", String.valueOf(startTime));
+        redisTemplate.opsForHash().put(key, userId + "-elapsedTime", String.valueOf(elapsedTime));
+        redisTemplate.opsForHash().put(key, userId + "-isRunning", "true");
+    }
+
+    private void stopTimer(Long roomId, Long userId) {
+        String key = "timer-room-" + roomId;
+
+        long totalElapsedTime = getTotalElapsedTime(userId, key);
+        updateElapsedTime(userId, key, totalElapsedTime);
+
+        redisTemplate.opsForHash().delete(key, userId + "-startTime");
+        redisTemplate.opsForHash().put(key, userId + "-isRunning", "false");
+    }
+
+    private void updateElapsedTime(Long userId, String key, long totalElapsedTime) {
+        //방에서 총 경과 시간
+        redisTemplate.opsForHash().put(key, userId + "-elapsedTime", String.valueOf(totalElapsedTime));
+
+        //오늘 유저의 모든 방 총 경과 시간
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND_EXCEPTION));
+        user.updateTodayAddedTime(totalElapsedTime);
+        userRepository.save(user);
+    }
+
+    private long getTotalElapsedTime(Long userId, String key) {
+        String startTimeStr = (String) redisTemplate.opsForHash().get(key, userId + "-startTime");
+        String elapsedTimeStr = (String) redisTemplate.opsForHash().get(key, userId + "-elapsedTime");
+        if (startTimeStr == null) {
+            throw new IllegalArgumentException("Timer not started for userId: " + userId);
+        }
+
+        long startTime = Long.parseLong(startTimeStr);
+        long elapsedTime = (elapsedTimeStr != null) ? Long.parseLong(elapsedTimeStr) : 0;
+
+        // 현재까지 경과 시간 계산
+        long currentElapsedTime = (System.currentTimeMillis() - startTime) / 1000; // 초 단위
+        long totalElapsedTime = elapsedTime + currentElapsedTime;
+        return totalElapsedTime;
     }
 }
 
