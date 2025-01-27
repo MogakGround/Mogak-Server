4bf0a740775e67e7887d60db2eebbe02184f4ef0
pkl0912 <pklpkl0912@gmail.com>
2025-01-27 19:16:40 +0900

    [feat] 모각작 내에서의 내 상태 (왼쪽 상단바) 기능 추가

--- src/main/java/com/example/mogakserver/roomuser/api/controller/RoomUserController.java
+++ src/main/java/com/example/mogakserver/roomuser/api/controller/RoomUserController.java
@@ -2,9 +2,11 @@ package com.example.mogakserver.roomuser.api.controller;
 
 import com.example.mogakserver.common.exception.dto.ErrorResponse;
 import com.example.mogakserver.common.exception.dto.SuccessNonDataResponse;
+import com.example.mogakserver.common.exception.dto.SuccessResponse;
 import com.example.mogakserver.common.exception.enums.SuccessCode;
 import com.example.mogakserver.common.util.resolver.user.UserId;
 import com.example.mogakserver.room.application.response.ScreenShareUsersListDTO;
+import com.example.mogakserver.roomuser.application.response.MyStatusResponseDTO;
 import com.example.mogakserver.roomuser.application.service.RoomUserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -38,5 +40,20 @@ public class RoomUserController {
     ) {
         roomUserService.updateIsScreenShareLargeAllowed(userId, roomId);
         return SuccessNonDataResponse.success(SuccessCode.UPDATE_SCREEN_SHARE_LARGE_SUCCESS);
+    }
+    @Operation(summary = "[JWT] 모각작 방 안에서의 내 상태 조회", description = "해당 방에서 사용자의 화면공유, 타이머 등의 상태 조회 api입니다")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "모각작 방에서 내 상태 조회 성공", content = @Content(schema = @Schema(implementation = ScreenShareUsersListDTO.class))),
+            @ApiResponse(responseCode = "404", description = "유저가 존재하지 않습니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @SecurityRequirement(name = "JWT Auth")
+    @GetMapping("/{roomId}/mystatus")
+    public SuccessResponse<MyStatusResponseDTO> getMyStatusInRoom(
+            @Parameter(hidden = true) @UserId Long userId,
+            @Parameter(name = "roomId", description = "방 id") @PathVariable(value = "roomId") Long roomId
+    ) {
+        MyStatusResponseDTO response = roomUserService.getStatus(userId, roomId);
+        return SuccessResponse.success(SuccessCode.GET_MY_STATUS_IN_ROOM_SUCCESS, response);
     }
 }
--- src/main/java/com/example/mogakserver/roomuser/application/service/RoomUserService.java
+++ src/main/java/com/example/mogakserver/roomuser/application/service/RoomUserService.java
@@ -2,9 +2,13 @@ package com.example.mogakserver.roomuser.application.service;
 
 import com.example.mogakserver.common.exception.enums.ErrorCode;
 import com.example.mogakserver.common.exception.model.NotFoundException;
+import com.example.mogakserver.roomuser.application.response.MyStatusResponseDTO;
 import com.example.mogakserver.roomuser.domain.entity.RoomUser;
 import com.example.mogakserver.roomuser.infra.repository.JpaRoomUserRepository;
+import com.example.mogakserver.user.domain.entity.User;
+import com.example.mogakserver.user.infra.repository.JpaUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +17,60 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class RoomUserService {
     private final JpaRoomUserRepository roomUserRepository;
+    private final JpaUserRepository userRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public void updateIsScreenShareLargeAllowed(Long userId, Long roomId){
         RoomUser roomUser = roomUserRepository.findByUserIdAndRoomId(userId, roomId).orElseThrow(()->new NotFoundException(ErrorCode.NOT_FOUND_ROOM_EXCEPTION));
         roomUser.updateIsVideoLargeAllowed();
     }
+
+    public MyStatusResponseDTO getStatus(Long userId, Long roomId) {
+        String timerKey = "timer-room-" + roomId;
+        String screenShareKey = "screen-share-room-" + roomId;
+
+        String nickName = getNickName(userId);
+
+        boolean isTimerRunning = isTimerRunning(userId, timerKey);
+
+        long elapsedTime = getElapsedTime(userId, timerKey);
+
+        int hour = (int) (elapsedTime / 3600);
+        int min = (int) ((elapsedTime % 3600) / 60);
+        int sec = (int) (elapsedTime % 60);
+
+        boolean isScreenSharing = redisTemplate.opsForSet().isMember(screenShareKey, String.valueOf(userId));
+
+        RoomUser roomUser = roomUserRepository.findByUserIdAndRoomId(userId, roomId).orElseThrow(()->new NotFoundException(ErrorCode.NOT_FOUND_ROOM_EXCEPTION));
+
+        return MyStatusResponseDTO.builder()
+                .nickName(nickName)
+                .isScreenSharing(isScreenSharing)
+                .isScreenAllowedLarge(roomUser.isVideoLargeAllowed())
+                .isTimerRunning(isTimerRunning)
+                .hour(hour)
+                .min(min)
+                .sec(sec)
+                .build();
+    }
+
+    private long getElapsedTime(Long userId, String timerKey) {
+        String elapsedTimeStr = (String) redisTemplate.opsForHash().get(timerKey, userId + "-elapsedTime");
+        long elapsedTime = (elapsedTimeStr != null) ? Long.parseLong(elapsedTimeStr) : 0;
+        return elapsedTime;
+    }
+
+    private boolean isTimerRunning(Long userId, String timerKey) {
+        String isRunningStr = (String) redisTemplate.opsForHash().get(timerKey, userId + "-isRunning");
+        boolean isTimerRunning = "true".equals(isRunningStr);
+        return isTimerRunning;
+    }
+
+    private String getNickName(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND_EXCEPTION));
+        String nickName = user.getNickName();
+        return nickName;
+    }
 }
+
