2c5c17850bce433470266e3ed338165fc3ebc01c
pkl0912 <pklpkl0912@gmail.com>
2025-01-31 21:19:14 +0900

    [feat] 랭킹 조회 api 기능 추가

--- src/main/java/com/example/mogakserver/user/api/controller/UserController.java
+++ src/main/java/com/example/mogakserver/user/api/controller/UserController.java
@@ -1,4 +1,57 @@
 package com.example.mogakserver.user.api.controller;
 
+import com.example.mogakserver.common.exception.dto.ErrorResponse;
+import com.example.mogakserver.common.exception.dto.SuccessNonDataResponse;
+import com.example.mogakserver.common.exception.dto.SuccessResponse;
+import com.example.mogakserver.common.exception.enums.SuccessCode;
+import com.example.mogakserver.common.util.resolver.user.UserId;
+import com.example.mogakserver.room.application.response.ScreenShareUsersListDTO;
+import com.example.mogakserver.user.application.response.RankingDTO;
+import com.example.mogakserver.user.application.response.RankingListDTO;
+import com.example.mogakserver.user.application.service.UserRankingScheduler;
+import com.example.mogakserver.user.application.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    private final UserRankingScheduler userRankingScheduler;
+    @Operation(summary = "[JWT] 랭킹 리스트 조회", description = "랭킹 리스트 조회 api 입니다")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "랭킹 리스트 조회 성공", content = @Content(schema = @Schema(implementation = ScreenShareUsersListDTO.class))),
+            @ApiResponse(responseCode = "404", description = "유저가 존재하지 않습니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @SecurityRequirement(name = "JWT Auth")
+    @GetMapping("/rankings")
+    public SuccessResponse<RankingListDTO> getRankingList(
+            @Parameter(hidden = true) @UserId Long userId,
+            @Parameter(name = "page", description = "페이지 ") @RequestParam(value = "page") int page,
+            @Parameter(name = "size", description = "페이지 ") @RequestParam(value = "size") int size
+    ) {
+        return SuccessResponse.success(SuccessCode.GET_RANKING_SUCCESS, userService.getPagedRanking(page, size));
+    }
+    @Operation(summary = "[JWT] 내 랭킹 조회", description = "내 랭킹 조회 api 입니다")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "랭킹 조회 성공", content = @Content(schema = @Schema(implementation = ScreenShareUsersListDTO.class))),
+            @ApiResponse(responseCode = "404", description = "유저가 존재하지 않습니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @SecurityRequirement(name = "JWT Auth")
+    @GetMapping("/my/ranking")
+    public SuccessResponse<RankingDTO> getMyranking(
+            @Parameter(hidden = true) @UserId Long userId
+    ) {
+        return SuccessResponse.success(SuccessCode.GET_RANKING_SUCCESS, userService.getUserRanking(userId));
+    }
 }
